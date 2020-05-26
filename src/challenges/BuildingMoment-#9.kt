4c5ed13d73db73e09599a36334fa17b0af4f86cc
Mattia Ferigutti <matt88developer@gmail.com>
2020-05-26 13:55:29 +0200

    New challenges have been added

--- src/challenges/BuildingMoment-#9.kt
+++ src/challenges/BuildingMoment-#9.kt
@@ -1,5 +1,36 @@
 package challenges
 
-class Circle(val radius: Double) {
+import kotlin.math.PI
+import kotlin.math.pow
 
+class Circle {
+
+    private var radius: Double? = null
+
+    fun getValue() {
+        println("Insert the radius value: ")
+        val stringInput = readLine()!!
+
+        try {
+            radius = stringInput.toDouble()
+            println("The area is ${area()}")
+            println("The circumference is ${circumference()}")
+        } catch (e: NumberFormatException) {
+            println("Insert a number")
+            getValue()
+        }
+    }
+
+    private fun area() : Double {
+        return PI * radius!!.pow(2)
+    }
+
+    private fun circumference() : Double {
+        return 2 * radius!! * PI
+    }
+}
+
+fun main() {
+    val circle = Circle()
+    circle.getValue()
 }

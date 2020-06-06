5f20b2ccee677e45749bbbb6d9038dfaeb5cfe6a
Mattia Ferigutti <mattiaferigutti@gmail.com>
2020-06-06 17:16:31 +0200

    Update ReadMe.kt

--- src/challenges/ReadMe.kt
+++ src/challenges/ReadMe.kt
@@ -7,7 +7,7 @@ package challenges
  * this course. Keep in mind that there are lots of ways to solve a problem and this is
  * the awesome thing about coding. You can look at these challenges as logic games.
  * Before jumping to the solution try to solve these challenges by yourself, you can
- * searching information on the internet o asking for a hint inside out Telegram group.
+ * searching information on the internet or asking for a hint inside our Telegram group.
  *
  * Good luck:)
  */
